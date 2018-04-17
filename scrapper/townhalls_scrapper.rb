710bc71457c47782dfdab8fca0a1bd5391bf8fcb
Cyril <manda.cyril@gmail.com>
2018-04-17 17:07:25 +0200

    Scrapper fonctionne pour les départements avec plusieurs pages de communes

--- scrapper/townhalls_scrapper.rb
+++ scrapper/townhalls_scrapper.rb
@@ -18,39 +18,50 @@ def get_the_email_of_a_townhal_from_its_webpage (townhall_url)
 	department = page.xpath('/html/body/div[1]/main/section[4]/div/table/tbody/tr[1]/td[2]').text
 	# Champ mail_adress ciblé avec l'inspecteur d'élément
 	mail_adress = page.xpath('/html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-	return name_of_town, department, mail_adress
+	return name_of_town, mail_adress
 end
 
 # Fonction get_all_the_urls_of_calvados_townhalls
 # @params : none
 # return : 
 #   array_of_URL : un tableau avec les URLs des pages de mairie
-def get_all_the_urls_of_calvados_townhalls
+def get_all_the_urls_of_townhalls(name_departement)
 	# Etant donné qu'il est utilisé à deux reprises, on stocke l'URL de base
+	array_of_URL = []
 	base_url = "http://annuaire-des-mairies.com"
 	# URL de la page répertoriant les communes
-	page = Nokogiri::HTML(open(base_url+"/calvados.html"))
-	# On récupère l'adresse de la page. Les liens étant en .commune, on utilise d'abord la base de l'URL, 
-	# on enlève le point du lien([1..-1]), puis on rajoute la partie correspondante à la commune. 
-	array_of_URL = page.xpath('//a[@class = "lientxt"]').map { |node| base_url + node.attributes["href"].value[1..-1] }
+	page = Nokogiri::HTML(open(base_url+"/#{name_departement}.html"))
+	list_img = page.xpath('//a/img')
+	list_url_pages = list_img.map { |element| base_url+"/"+element.parent["href"]}
+	
+	list_url_pages.each{ |url|
+		page = Nokogiri::HTML(open(url)) unless page == (base_url+"/#{name_departement}.html")
+		# On récupère l'adresse de la page. Les liens étant en base commune, on utilise d'abord la base de l'URL, 
+		# on enlève le point du lien([1..-1]), puis on rajoute la partie correspondante à la commune. 
+		array_of_URL.concat(page.xpath('//a[@class = "lientxt"]').map { |node| base_url + node.attributes["href"].value[1..-1] })
+	}
 	return array_of_URL		# Tableau des URLs
 end
 
 # Fonction get_name_email_of_calvados
 # @params : none
 # return : 
-#   array_of_URL : un tableau avec les URLs des pages de mairie
-def get_name_email_of_calvados
+#   array_of_hash_of_URL : un tableau avec hash comprenant les noms des villes, des URLs des pages de mairie
+def get_name_email_of_departement(name_departement)
 	result = []
+	name_departement.downcase!
 	# On récupère la liste des URLs des pages de chaque ville
-	list_url = get_all_the_urls_of_calvados_townhalls
+	list_url = get_all_the_urls_of_townhalls(name_departement)
 	# Pour chaque commune, on utilise la fonction pour récupérer le nom et le mail de la commune, puis on range les informations dans un tableau de hash
-	list_url.each {|town_URL| name, depart, mail = get_the_email_of_a_townhal_from_its_webpage(town_URL); result.push({:name => name.to_s, :department => depart.to_s, :email => mail})}
+	list_url.each {|town_URL| 
+		name, mail = get_the_email_of_a_townhal_from_its_webpage(town_URL)
+		result.push({:name => name.to_s, :department => name_departement.to_s, :email => mail})
+	}
 	return result
 end
 
 def perform
-	puts get_name_email_of_calvados
+	puts get_name_email_of_departement("Calvados")
 end
 
 perform
