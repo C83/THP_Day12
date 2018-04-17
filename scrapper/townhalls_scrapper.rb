fdd25ee963109adb5c7592948435dfa0efdb6b09
Itspacome <pacome.perrichot@gmail.com>
2018-04-17 16:04:06 +0200

    new scapping

--- scrapper/townhalls_scrapper.rb
+++ scrapper/townhalls_scrapper.rb
@@ -1,56 +1,35 @@
-require 'pry'
 require 'rubygems'
-require 'nokogiri'  
+require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-# Fonction get_the_email_of_a_townhall_from_its_webpage
-# @params : 
-# 	townhall_url : la page de la mairie
-# return : 
-#   name_of_town : le nom de la ville
-#   mail_adress : l'adresse mail de la mairie
-def get_the_email_of_a_townhal_from_its_webpage (townhall_url)
-	page = Nokogiri::HTML(open(townhall_url))   
-	# Chaque page de mairie contient un lien vers la page elle-même, avec comme ancre le nom de la ville. On prend donc le texte correspondant 
-	name_of_town_with_zip_code = page.xpath('/html/body/div[1]/main/section[1]/div/div/div/h1').text.split (" - ") # On sépare le nom de la ville et le code postale
-	name_of_town = name_of_town_with_zip_code[0] # on récupére le nom de la ville seulement
-	# Champ département ciblé avec l'inspecteur d'élément
-	department = page.xpath('/html/body/div[1]/main/section[4]/div/table/tbody/tr[1]/td[2]').text
-	# Champ mail_adress ciblé avec l'inspecteur d'élément
-	mail_adress = page.xpath('/html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-	return name_of_town, department, mail_adress
+def townhall (link_townhall)
+  page = Nokogiri::HTML(open(link_townhall))
+  townhall_name_and_zip = page.css("div[1]/main/section[1]/div/div/div/h1").text.split(" - ")
+  townhall_name = townhall_name_and_zip[0]
+  townhall_zip = townhall_name_and_zip[1]
+  department = page.xpath('/html/body/div[1]/main/section[4]/div/table/tbody/tr[1]/td[2]').text
+  townhall_mail = page.css("div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]").text
+  hash_townhall = Hash[:name => townhall_name, :department => department, :email => townhall_mail]
+  return hash_townhall
 end
 
-# Fonction get_all_the_urls_of_calvados_townhalls
-# @params : none
-# return : 
-#   array_of_URL : un tableau avec les URLs des pages de mairie
-def get_all_the_urls_of_calvados_townhalls
-	# Etant donné qu'il est utilisé à deux reprises, on stocke l'URL de base
-	base_url = "http://annuaire-des-mairies.com"
-	# URL de la page répertoriant les communes
-	page = Nokogiri::HTML(open(base_url+"/calvados.html"))
-	# On récupère l'adresse de la page. Les liens étant en .commune, on utilise d'abord la base de l'URL, 
-	# on enlève le point du lien([1..-1]), puis on rajoute la partie correspondante à la commune. 
-	array_of_URL = page.xpath('//a[@class = "lientxt"]').map { |node| base_url + node.attributes["href"].value[1..-1] }
-	return array_of_URL		# Tableau des URLs
-end
-
-# Fonction get_name_email_of_calvados
-# @params : none
-# return : 
-#   array_of_URL : un tableau avec les URLs des pages de mairie
-def get_name_email_of_calvados
-	result = []
-	# On récupère la liste des URLs des pages de chaque ville
-	list_url = get_all_the_urls_of_calvados_townhalls
-	# Pour chaque commune, on utilise la fonction pour récupérer le nom et le mail de la commune, puis on range les informations dans un tableau de hash
-	list_url.each {|town_URL| name, depart, mail = get_the_email_of_a_townhal_from_its_webpage(town_URL); result.push({:name => name.to_s, :department => depart.to_s, :email => mail})}
-	return result
+def scan_list_townhall (link_url)
+  url_origin = "http://annuaire-des-mairies.com/"
+  list_townhall = []
+  page_origin = Nokogiri::HTML(open(link_url))
+  link_town = page_origin.css('a.lientxt')
+  link_town.each {|x|
+    link = x['href']
+    link_to_town = URI.join(url_origin, link).to_s
+    list_townhall.push(townhall(link_to_town))
+  }
+  return list_townhall
 end
 
 def perform
-	puts get_name_email_of_calvados
+  url_origin = "http://annuaire-des-mairies.com/calvados.html"
+  puts scan_list_townhall(url_origin)
 end
 
 perform
